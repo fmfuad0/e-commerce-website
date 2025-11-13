029adfe4c1589da3b572eae07e79eeae8efa5445
fmfuad0 <fartinfuad@gmail.com>
2025-11-14 03:59:26 +0600

    Edit Readme

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import {Button} from "@mui/material";
 
 function Banner({image, position, tag, title , price}) {
     return (
-        <div className="relative group overflow-hidden h-full w-full flex rounded-lg bg-cover bg-no-repeat bg-center justify-end items-center py-12"
+        <div className="relative text-black group overflow-hidden h-full w-full flex rounded-lg bg-cover bg-no-repeat bg-center justify-end items-center py-12"
         >
             {/* Background layer */}
             <div
@@ -14,10 +14,10 @@ function Banner({image, position, tag, title , price}) {
             {position==='left'? <div className="w-[40%] z-10"></div> : <></>}
             <div className={`w-[60%] gap-1 flex flex-col ${position==='right'? "" : "text-center "} align-middle pl-10  z-10`}>
 
-                <p className="text-text font-[600]  leading-8 text-sm">
+                <p className="font-[600] text-black leading-8 text-sm">
                     {tag}
                 </p>
-                <p className="text-text font-[600] max-w-[280px] leading-8 text-xl">
+                <p className="font-[600] max-w-[280px] leading-8 text-xl">
                     {title}
                 </p>
                 <h1 className="text-primary text-xl font-[700]">{price? `$${price}`  : ""}</h1>
