404cee11d6679670d2094d02f97a8358a77e887a
fmfuad0 <fartinfuad@gmail.com>
2025-11-15 03:51:53 +0600

    k

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,9 @@ import ListProducts from "./pages/ListProducts.jsx";
 import ViewProduct from "./pages/ViewProduct.jsx";
 import Cart from "./components/Cart.jsx";
 import {CartProvider} from "./contexts/CartContext.jsx";
-import Orders from "./components/Orders.jsx";
-import OrderConfirmation from "./components/OrderConfirmation.jsx";
 import Checkout from "./pages/Checkout.jsx";
 import {AuthProvider} from "./contexts/AuthContext.jsx";
+
 
 export default function App() {
     return (
